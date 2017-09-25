11c34198553d19e29de82a31adadf450a8838877
Pavel Nishcharetau <alzarlion@gmail.com>
2017-09-25 09:13:02 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,96 @@
+(function () {
+    'use strict';
+
+    const listOfPublications = angular.module('listOfPublications', []);
+    listOfPublications.filter('startFrom', function () {
+       return function (input, start) {
+           start = +start;
+           return input.slice(start);
+       } 
+    });
+    listOfPublications.controller('listOfPublicationsCtrl', function ($scope, $http) {
+        $http({
+            method: 'GET',
+            url: '/entry'
+        }).then(function (success) {
+            console.log("I got the data I requested");
+            $scope.data = success.data;
+            $scope.contact = null;
+        }, function (error) {
+            console.log("Error");
+        });
+
+
+        $scope.addInformation = function () {
+            if($scope.data.length) {
+                $scope.contact.nameUser = $scope.data[0].nameUser;
+                $scope.contact.city = $scope.data[0].city;
+                $scope.contact.country = $scope.data[0].country;
+            } else {
+                $scope.contact.nameUser = 'User';
+                $scope.contact.city = 'Paris';
+                $scope.contact.country = 'French';
+            }
+
+            const dateTime = new Date();
+
+            const options = {
+                era: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                weekday: 'long',
+                timezone: 'UTC',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric'
+            };
+
+            $scope.contact.date = dateTime.toLocaleString("ru", options);
+
+            $scope.contact.commentCounter = '0';
+
+            $http.post('/entry', $scope.contact);
+        };
+
+
+        $scope.remove = function(id) {
+            console.log(id);
+            $http.delete('/entry/' + id);
+        };
+
+        $scope.currentPage = 0;
+        $scope.itemsPerPage = 5;
+        $scope.items = [];
+
+        for(let i = 0; i < 25; i++) {
+            $scope.items.push('Product ' + 1);
+        }
+
+        $scope.firstPage = function () {
+            return $scope.currentPage == 0;
+        };
+
+        $scope.lastPage = function () {
+            let lastPageNum = Math.ceil($scope.items.length / $scope.itemsPerPage - 1);
+            return $scope.currentPage == lastPageNum;
+        };
+
+        $scope.numberOfPages = function () {
+            return Math.ceil($scope.items.length / $scope.itemsPerPage);
+        };
+
+        $scope.startingItem = function () {
+            return $scope.currentPage * $scope.itemsPerPage;
+        };
+
+        $scope.pageBack = function () {
+            $scope.currentPage = $scope.currentPage - 1;
+        };
+
+        $scope.pageForward = function () {
+            $scope.currentPage = $scope.currentPage + 1;
+        };
+    });
+})();
+
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongojs from 'mongojs';
+import bodyParser from 'body-parser';
+const app = express();
+const db = mongojs('entry', ['entry', 'user']);
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+app.get('/entry', function (req, res) {
+    db.entry.find(function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+app.post('/entry', function (req, res) {
+    db.entry.insert(req.body, function(err, doc) {
+        res.json(doc);
+    });
+});
+
+app.delete('/entry/id', function (req, res) {
+    const id = req.params.id;
+    console.log(id);
+    db.entry.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+const server = app.listen(3000, () => {
+    console.log("Server is up and running on port 3000");
+});
